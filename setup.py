a3238a58632ab9d0de578abb53be582cff9dd1c1
Travis Cunningham <travcunn@iupui.edu>
2015-01-12 13:58:24 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='queues',
+      version='0.6.2',
+      description='A lowest-common-denominator API for interacting with lightweight queue services.',
+      author='Matt Croydon',
+      author_email='mcroydon@gmail.com',
+      url='http://code.google.com/p/queues/',
+      packages=['queues', 'queues.backends'],
+      py_modules=['test'],
+      package_dir={'queues': 'queues'},
+     )
